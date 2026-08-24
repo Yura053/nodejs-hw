506879019a4531cb35d5630446b8e10f647d0a9d
Yurii Ivanes <ivanecura82@gmail.com>
2026-08-24 23:56:25 +0300

    Implement MongoDB CRUD for notes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+import pino from 'pino-htpp';
+import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { Student } from './models/student.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/studentsRoutes.js';
+
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Глобальні middleware
+app.use(logger);         // 1. Логер першим — бачить усі запити
+app.use(express.json()); // 2. Парсинг JSON-тіла
+app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+// підключаємо групу маршрутів студента
+app.use(notesRoutes);
+// 404 і обробник помилок — наприкінці ланцюжка
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
@@ -25,6 +43,11 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+
+
